4e901423e4f7ff68f16a5ebf82e0191192908113
Amit Kumar <amitkumar6k60@gmail.com>
2024-11-07 06:49:59 +0530

    uploading backend

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const userRoutes = require('./routes/userRoutes');
+const matchRoutes = require('./routes/matchRoutes');
+const announcementRoutes = require('./routes/announcementRoutes');
+
+const cors = require('cors');
+
+require('dotenv').config();
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/matches', matchRoutes);
+app.use('/api/announcements', announcementRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
